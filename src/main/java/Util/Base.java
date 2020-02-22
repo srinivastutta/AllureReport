edc89134f8fd9cd8513e37389dde9c5a535c5b7b
Srinivas <srinivastutta@gmail.com">
2020-02-22 20:02:57 +0100

    xx

--- src/main/java/Util/Base.java
+++ src/main/java/Util/Base.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class Base {
 	public WebDriver driver;
 	public Properties prop;
